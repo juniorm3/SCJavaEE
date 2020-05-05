3c1cf2685874e6559aaa5e3fc45086e4f89e08fe
Mario <mario.martins@sonda.com>
2020-05-05 17:19:58 -0300

    Prototipando a pesquisa de pedidos

---
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/PesquisaPedidosBean.java
@@ -0,0 +1,25 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PesquisaPedidosBean {
+
+	private List<Integer> pedidosFiltrados;
+	
+	public PesquisaPedidosBean() {
+		pedidosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			pedidosFiltrados.add(i);
+		}
+	}
+	
+	public List<Integer> getProdutosFiltrados() {
+		return pedidosFiltrados;
+	}
+}
