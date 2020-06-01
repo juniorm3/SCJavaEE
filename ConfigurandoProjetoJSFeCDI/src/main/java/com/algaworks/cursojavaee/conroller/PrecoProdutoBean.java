8e30fb5e9895972e1de34c77df5f52dc0b66936f
Mario <mario.martins@sonda.com>
2020-06-01 16:49:58 -0300

    Configurando um projeto com JSF e CDI

---
+++ ConfigurandoProjetoJSFeCDI/src/main/java/com/algaworks/cursojavaee/conroller/PrecoProdutoBean.java
@@ -0,0 +1,17 @@
+package com.algaworks.cursojavaee.conroller;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.cursojavaee.service.CalculadoraPreco;
+
+@Named("meuBean")
+public class PrecoProdutoBean {
+
+	@Inject
+	private CalculadoraPreco calculadora;
+	
+	public double getPreco() {
+		return calculadora.calcularPreco(12, 44.55);
+	}
+}
---
+++ ConfigurandoProjetoJSFeCDI/src/main/java/com/algaworks/cursojavaee/service/CalculadoraPreco.java
@@ -0,0 +1,8 @@
+package com.algaworks.cursojavaee.service;
+
+public class CalculadoraPreco {
+
+	public double calcularPreco(int quantidade, double precoUnitario) {
+		return quantidade * precoUnitario;
+	}
+}
