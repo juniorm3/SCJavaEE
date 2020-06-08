1e2ec9cbbd4499b9131fd6b96a2886c686353f87
Mario <mario.martins@sonda.com>
2020-06-08 17:40:20 -0300

    Qualificadores

---
+++ Qualificadores/src/main/java/com/algaworks/cursojavaee/controller/EnvioMensagemBean.java
@@ -0,0 +1,32 @@
+package com.algaworks.cursojavaee.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.cursojavaee.service.Mensageiro;
+import com.algaworks.cursojavaee.service.Urgente;
+
+@Named
+@RequestScoped
+public class EnvioMensagemBean {
+
+	@Inject @Urgente //@Default
+	private Mensageiro mensageiro;
+
+	private String texto;
+
+	public void enviarMensagem() {
+		mensageiro.enviarMensagem(texto);
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+}
